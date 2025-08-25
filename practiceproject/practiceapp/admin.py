5374e94ad9b992b46cc6a4ddb8610b5ff3f51908
Sadhana Sapkota <86416858+sadhanasapkotaa@users.noreply.github.com>
2025-08-25 13:19:17 +0000

    Added Order and OrderItem to admin

--- practiceproject/practiceapp/admin.py
+++ practiceproject/practiceapp/admin.py
@@ -1,3 +1,7 @@
+"""Imports for admin"""
 from django.contrib import admin
+from .models import OrderItem, Order
 
 # Register your models here.
+admin.site.register(Order)
+admin.site.register(OrderItem)
